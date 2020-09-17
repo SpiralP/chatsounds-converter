fd92c70e5380f1705e07992854cb310c6a33f348
SpiralP <spiral.palmer@gmail.com>
2020-09-16 21:20:19 -0700

    yeehaw

--- app/App.tsx
+++ app/App.tsx
@@ -1,36 +1,71 @@
 import FileSaver from "file-saver";
 import { Checkbox } from "@blueprintjs/core";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useMemo } from "react";
 import { useDropzone } from "react-dropzone";
+import Promise from "bluebird";
+
+const baseStyle: React.CSSProperties = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+};
+
+const activeStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
 
 function FileDropzone({ stereo }) {
   const [uploading, setUploading] = useState(false);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState("");
 
-  const onDrop = useCallback((files) => {
-    const file = files[0];
-    const fileName = file.name.replace(/\.[^/.]+$/, "");
-    console.log(fileName);
-
+  const onDrop = useCallback((files: File[]) => {
     setUploading(true);
-    fetch("/convert?stereo=" + (stereo ? "1" : "0"), {
-      method: "POST",
-      body: file,
-    }).then(
-      async (response) => {
-        console.log(response);
+
+    Promise.map(
+      files,
+      async (file) => {
+        const fileName = file.name.replace(/\.[^/.]+$/, "");
+
+        const response = await fetch(
+          "/convert?stereo=" + (stereo ? "1" : "0"),
+          {
+            method: "POST",
+            body: file,
+          }
+        );
+
         if (response.status === 200) {
           const blob = await response.blob();
-          console.log(blob.size);
           if (blob.size !== 0) {
             FileSaver.saveAs(blob, fileName + ".ogg");
           }
         } else {
           const message = await response.text();
-          console.warn(message);
-          setError(message);
+          console.warn(fileName, message);
+          setError(`Error converting ${fileName}: ${message}`);
         }
-
+      },
+      { concurrency: 2 }
+    ).then(
+      () => {
         setUploading(false);
       },
       () => {
@@ -38,24 +73,40 @@ function FileDropzone({ stereo }) {
       }
     );
   }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({ onDrop });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isDragActive, isDragReject, isDragAccept]
+  );
 
   return (
-    <>
-      {error != null ? `Error uploading: ${error}` : null}
+    <div>
+      {error}
       {uploading ? (
-        <div>uploading...</div>
+        <h1>Converting...</h1>
       ) : (
-        <div {...getRootProps()}>
+        <div {...getRootProps({ style })}>
           <input {...getInputProps()} />
           {isDragActive ? (
-            <p>Drop the files here ...</p>
+            <h1>Drop the files here ...</h1>
           ) : (
-            <p>Drag 'n' drop some files here, or click to select files</p>
+            <h1>Drag 'n' drop some files here, or click to select files</h1>
           )}
         </div>
       )}
-    </>
+    </div>
   );
 }
 
@@ -63,7 +114,9 @@ export default function App() {
   const [stereo, setStereo] = useState(false);
 
   return (
-    <>
+    <div style={{ margin: "24px" }}>
+      <FileDropzone stereo={stereo} />
+      <br />
       <Checkbox
         label="Stereo"
         checked={stereo}
@@ -71,8 +124,6 @@ export default function App() {
           setStereo((event.target as HTMLInputElement).checked);
         }}
       />
-
-      <FileDropzone stereo={stereo} />
-    </>
+    </div>
   );
 }
