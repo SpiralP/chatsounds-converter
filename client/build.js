edb3489f3ff2ce80f2a57661864aee1f0139e494
SpiralP <spiral.palmer@gmail.com>
2022-03-24 18:14:48 -0700

    better NODE_ENV in handling

--- client/build.js
+++ client/build.js
@@ -1,12 +1,16 @@
+const NODE_ENV = process.env.NODE_ENV;
+
 require("esbuild").buildSync({
   entryPoints: ["src/index.tsx"],
+  outfile: "dist/index.js",
   bundle: true,
-  minify: true,
   sourcemap: true,
+  minify: NODE_ENV === "production",
+  platform: "browser",
   target: ["chrome88", "firefox85", "safari14", "edge88"],
   outfile: "dist/index.js",
   define: {
-    "process.env.NODE_ENV": '"production"',
-    "process.env": '{"NODE_ENV": "production"}',
+    "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
+    "process.env": JSON.stringify({ NODE_ENV }),
   },
 });
--- server/build.js
+++ server/build.js
@@ -1,13 +1,17 @@
-const production = process.env.NODE_ENV === "production";
+const NODE_ENV = process.env.NODE_ENV;
 
 require("esbuild").buildSync({
   entryPoints: ["src/index.ts"],
   outfile: "dist/index.js",
   bundle: true,
   sourcemap: true,
-  minify: production,
+  minify: NODE_ENV === "production",
   platform: "node",
   target: "node16",
   format: "cjs",
   external: ["express", "ffmpeg-static", "fluent-ffmpeg", "@ffmpeg-installer"],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
+    "process.env": JSON.stringify({ NODE_ENV }),
+  },
 });
