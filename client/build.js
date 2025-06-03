950c50188d0e01174e6d4fd4c9957f4f15572f67
SpiralP <spiral.palmer@gmail.com>
2025-06-03 16:01:39 -0700

    build and host css

--- client/build.js
+++ client/build.js
@@ -2,7 +2,7 @@ const NODE_ENV = process.env.NODE_ENV || "development";
 
 require("esbuild").buildSync({
   entryPoints: ["src/index.tsx"],
-  outfile: "dist/index.js",
+  outdir: "dist",
   bundle: true,
   packages: "bundle",
   sourcemap: true,
@@ -11,5 +11,12 @@ require("esbuild").buildSync({
   target: ["chrome88", "firefox85", "safari14", "edge88"],
   define: {
     "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
+  },
+  loader: {
+    ".eot": "file",
+    ".svg": "file",
+    ".ttf": "file",
+    ".woff": "file",
+    ".woff2": "file",
   },
 });
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,7 @@
+import "normalize.css/normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
